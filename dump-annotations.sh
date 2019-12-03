b62d4b0bebb7417462e0d24ce3d9fb33a5145d9d
Benjmain Frazier <benjamin.frazier@gmail.com>
2019-12-03 14:29:35 -0500

    refactored into functions

--- dump-annotations.sh
+++ dump-annotations.sh
@@ -42,91 +42,69 @@ rm_csv_file() {
 }
 
 get_notes_query() {
+	local zassetid="$1"
 	local notes_query="SELECT ZANNOTATIONREPRESENTATIVETEXT as BroaderText, ZANNOTATIONSELECTEDTEXT as SelectedText, ZANNOTATIONNOTE as Note, ZFUTUREPROOFING5 as Chapter, ZANNOTATIONCREATIONDATE as Created, ZANNOTATIONMODIFICATIONDATE as Modified FROM ZAEANNOTATION WHERE ZANNOTATIONSELECTEDTEXT IS NOT NULL AND ZANNOTATIONASSETID = '"$zassetid"' ORDER BY ZANNOTATIONASSETID ASC,Created ASC"
 	echo "$notes_query"
+}
+
+get_notes_db_file() {
+	local notes_database_file=`find "$NOTES_DATABASE_DIRECTORY" -iname "*.sqlite"`
+	echo "$notes_database_file"
+}
+
+get_text_by_delimiter() {
+	local line="$1"
+	local delimiter="$2"
+	text=`echo $line | cut -f "$delimiter" -d '|'`
+	if [ -z ${text+x} ]; then
+		# echo "text is unset";
+		text="''"
+	else
+		if [[ "$delimiter" -gt 4 ]]; then
+			# echo "$delimiter is greater than 4"
+			# The iBooks DB stores the ZANNOTATIONCREATIONDATE and ZANNOTATIONMODIFICATIONDATE values in ISO-8601 standard
+			# as seconds from the date of 2000-01-01 00:00:00 +0000.
+			# For example,
+			# if I had a value of 588916720.882715 in the ZANNOTATIONCREATIONDATE field I could do the following to get the actual date that
+			# this value represents in human readable form:
+			# /usr/local/bin/gdate '+%m/%d/%Y %I:%M %p' --date="2000-01-01 00:00:00 +0000 + 588916720.882715 seconds"
+			text=`echo $text | cut -f 5 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 + 0000 + {} seconds'"`
+		else
+			# echo "text is set to '$text'"
+			text=`echo $line | cut -f "$delimiter" -d '|'`
+		fi
+	fi
+	echo "$text"
 }
 
 get_notes_info() {
 	# notes
 	local zassetid="$1"
 	local notes_query="$2"
-	# local notes_database_file=`find "$NOTES_DATABASE_DIRECTORY" -iname "*.sqlite"`
-	# The iBooks DB stores the ZANNOTATIONCREATIONDATE and ZANNOTATIONMODIFICATIONDATE values in ISO-8601 standard as seconds from the date of 2000-01-01 00:00:00 +0000.
-	# For example,
-	# if I had a value of 588916720.882715 in the ZANNOTATIONCREATIONDATE field I could do the following to get the actual date that
-	# this value represents in human readable form:
-	# /usr/local/bin/gdate '+%m/%d/%Y %I:%M %p' --date="2000-01-01 00:00:00 +0000 + 588916720.882715 seconds"
+	local notes_database_file="$3"
 	# ; is the delimiter
 	"SQLITE3" "$notes_database_file" "$notes_query" | while read line; do
+		# echo "Line: $line"
+		local border_text=$(get_text_by_delimiter "$line" 1)
+		local selected_text=$(get_text_by_delimiter "$line" 2)
+		local note_text=$(get_text_by_delimiter "$line" 3)
 
-		# broaderText
-		broaderText=`echo $line | cut -f 1 -d '|'`
-		if [ -z ${broaderText+x} ]; then
-			# echo "broaderText is unset";
-			broaderText="''"
-		else
-			# echo "broaderText is set to '$broaderText'";
-			broaderText=`echo $line | cut -f 1 -d '|'`
-		fi
-
-		# selectedText
-		selectedText=`echo $line | cut -f 2 -d '|'`
-		if [ -z ${selectedText+x} ]; then
-			# echo "selectedText is unset";
-			selectedText="''"
-		else
-			# echo "selectedText is set to '$selectedText'";
-			selectedText=`echo $line | cut -f 2 -d '|'`
-		fi
-
-		# note
-		note=`echo $line | cut -f 3 -d '|'`
-		if [ -z ${note+x} ]; then
-			# echo "note is unset";
-			note="''"
-		else
-			# echo "note is set to '$note'";
-			note=`echo $line | cut -f 3 -d '|'`
-		fi
-
-		## DATES
-		chapterLine=`echo $line | cut -f 4 -d '|'`
-		chapter=`echo $line | cut -f 4 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 + 0000 + {} seconds'"`
-		if [ -z ${chapter+x} ]; then
-			# echo "chapter is unset";
-			chapter="''"
-		else
-			chapter=`echo $line | cut -f 4 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 +0000 + {} seconds'"`
-		fi
-
-		created=`echo $line | cut -f 5 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 + 0000 + {} seconds'"`
-		if [ -z ${created+x} ]; then
-			# echo "created is unset";
-			created="''"
-		else
-			# echo "created is set to '$created'";
-			created=`echo $line | cut -f 5 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 + 0000 + {} seconds'"`
-		fi
-
-		modified=`echo $line | cut -f 6 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 + 0000 + {} seconds'"`
-		if [ -z ${modified+x} ]; then
-			# echo "modified is unset";
-			modified="''"
-		else
-			modified=`echo $line | cut -f 6 -d '|' | xargs -I {} sh -c "$GDATE '$GDATE_FORMAT' --date='2000-01-01 00:00:00 + 0000 + {} seconds'"`
-		fi
-		echo "$selectedText|Chapter: $chapter|Created: $created|Modified: $modified;back"
-		# echo "$selectedText|Chapter: $chapter|Created: $created|Modified: $modified;back" >> "$CSV_FILE"
+		# Dates
+		local chapter=$(get_text_by_delimiter "$line" 4)
+		local created=$(get_text_by_delimiter "$line" 5)
+		local modified=$(get_text_by_delimiter "$line" 6)
+		# echo "$selectedText|Chapter: $chapter|Created: $created|Modified: $modified;back"
+		echo "$selectedText|Chapter: $chapter|Created: $created|Modified: $modified;back" >> "$CSV_FILE"
 	done
-
 	echo "Done! Output file is @ $CSV_FILE"
 }
 
 main() {
 	id="$(get_ID "$1")"
 	rm_csv_file
-	notes_query=$(get_notes_query)
-	get_notes_info "$id" "$notes_query"
+	notes_query=$(get_notes_query "$id")
+	notes_db_file=$(get_notes_db_file)
+	get_notes_info "$id" "$notes_query" "$notes_db_file"
 }
 
 main "$1"
